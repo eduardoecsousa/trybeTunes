0f3f5e177a588e3679d8cace8db077e65e5fb4b7
Eduardo.Sousa <dudus.sousa4002@gmail.com>
2022-11-03 19:04:57 -0300

    requisito 3 concluido

--- src/Components/FormLogin.jsx
+++ src/Components/FormLogin.jsx
@@ -34,7 +34,7 @@ class FormLogin extends Component {
       loading: true,
     }, async () => {
       const { userName } = this.state;
-      await createUser({ nome: userName });
+      await createUser({ name: userName });
       this.setState({
         direct: true,
         loading: false,
@@ -55,21 +55,22 @@ class FormLogin extends Component {
               <label htmlFor="login-name-input">
                 UserName
                 <input
+                  data-testid="login-name-input"
+                  name="userName"
                   type="text"
                   id="login-name-input"
-                  testid="login-name-input"
                   value={ userName }
                   onChange={ this.handleChange }
                 />
-                <button
-                  type="submit"
-                  data-testid="login-submit-button"
-                  disabled={ disabled }
-                  onClick={ this.handleClick }
-                >
-                  Entrar
-                </button>
               </label>
+              <button
+                type="submit"
+                data-testid="login-submit-button"
+                disabled={ disabled }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
             </form>
           </div>)}
       </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const userName = await getUser();
+    this.setState({
+      name: userName.name,
+      loading: false,
+    });
+  }
+
   render() {
+    const { name, loading } = this.state;
     return (
-      <header data-testid="header-component">Header</header>
+      <header data-testid="header-component">
+        { loading ? <Loading /> : <p data-testid="header-user-name">{name}</p> }
+      </header>
     );
   }
 }
